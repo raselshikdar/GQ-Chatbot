efdbc1d0ffb9b3545ef5da6cc73d5bc5bf2ad6da
Rasel Shikdar <imrasel597@gmail.com>
2025-03-22 15:00:04 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'chatgpt-gray': '#343541',
+        'chatgpt-dark-gray': '#202123',
+        'chatgpt-light-gray': '#40414F',
+        'chatgpt-border': '#555767',
+        'chatgpt-green': '#19C37D',
+      },
+    },
   },
   plugins: [],
-};
+}
