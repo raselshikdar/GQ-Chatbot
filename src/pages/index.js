42d2c1e3b17970a32c65909672b5aa4fc421a6a4
Rasel Shikdar <imrasel597@gmail.com>
2025-03-22 17:06:18 +0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
-import ChatWindow from '../components/ChatWindow'
+import Head from 'next/head';
+import ChatWindow from '../components/ChatWindow';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-secondary-dark">
-      {/* Render the ChatWindow component */}
-      <ChatWindow />
-    </div>
-  )
+    <>
+      <Head>
+        <link id="favicon" rel="shortcut icon" href="/assets/images/favicon.webp" type="image/webp" />
+      </Head>
+      <div className="min-h-screen bg-secondary-dark">
+        {/* Render the ChatWindow component */}
+        <ChatWindow />
+      </div>
+    </>
+  );
 }
