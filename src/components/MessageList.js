4ffeb78fbaf3e395752b4e815d42b742362e84d0
Rasel Shikdar <imrasel597@gmail.com>
2025-03-22 14:46:43 +0600

    Update MessageList.js

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -2,10 +2,21 @@
 
 export default function MessageList({ messages }) {
   return (
-    <div className="message-list">
-      {messages.map((msg, index) => (
-        <div key={index} className={`message ${msg.type}`}>
-          <p>{msg.text}</p>
+    <div className="space-y-4">
+      {messages.map((msg, idx) => (
+        <div
+          key={idx}
+          className={`flex ${msg.sender === 'user' ? 'justify-end' : 'justify-start'}`}
+        >
+          <div
+            className={`px-4 py-2 rounded-lg max-w-xs break-words ${
+              msg.sender === 'user'
+                ? 'bg-blue-600 text-white'
+                : 'bg-gray-200 text-gray-800'
+            }`}
+          >
+            {msg.text}
+          </div>
         </div>
       ))}
     </div>
