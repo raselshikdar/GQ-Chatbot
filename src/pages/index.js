5df0317aab52477bb3c9bf09b2e37090672e47d9
Rasel Shikdar <imrasel597@gmail.com>
2025-03-22 15:49:10 +0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-// src/pages/index.js
-import ChatWindow from "../components/ChatWindow";
+import ChatWindow from '../components/ChatWindow'
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
+    <div className="min-h-screen bg-secondary-dark">
+      {/* Render the ChatWindow component */}
       <ChatWindow />
     </div>
-  );
+  )
 }
